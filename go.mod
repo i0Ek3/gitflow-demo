6a74b3c956f9266817007740afa477bcc5b6143a
i0Ek3 <kno30826@gmail.com>
2022-05-20 20:38:13 +0800

    fix wrong color

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/i0Ek3/gitflow-demo
 go 1.18
 
 require (
+	github.com/i0Ek3/color v0.0.0-20211226140939-808f4a3191e6 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
 )
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
     fmt.Println(color.Blue("This is a Git Flow test."))
     fmt.Println(color.Red("Please don't use it!"))
-    fmt.Println(color.Gray("What a taugh day!"))
+    fmt.Println(color.White("What a taugh day!"))
     fmt.Println(color.Yellow("This message created by @deadl10n."))
     log.Warnf("Add log by @i0Ek3.")
     fmt.Println(color.Colorize("cyan", "add new lib"))
